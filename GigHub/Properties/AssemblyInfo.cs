7b791edabe93b4bc7718875a7ec464fdfac6bead
Corey Sutton <coreysutton@outlook.co.nz>
2018-01-18 21:30:54 +1300

    Created project. Ran PM> Enable-Migarations

--- GigHub/Properties/AssemblyInfo.cs
+++ GigHub/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7f61277-91af-4dbf-8ab3-be77ee1ffcac")]
+[assembly: Guid("f2d2d584-e6c0-4ded-9acd-96f2d5bc582e")]
 
 // Version information for an assembly consists of the following four values:
 //
